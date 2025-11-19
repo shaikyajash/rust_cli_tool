72a9a3b6450af3f1c18c14a154f6091ca4ba1b8d
shaikyajash <shaikyajash@gmail.com>
2025-11-19 14:41:03 +0530

    broken down into functions

--- multirun/src/main.rs
+++ multirun/src/main.rs
@@ -1,6 +1,10 @@
 use clap::{Parser};
 use std::{
-    collections::VecDeque, io::{self, BufRead}, process, sync::{Arc, Mutex}, thread
+    collections::VecDeque,
+    io::{self, BufRead},
+    process,
+    sync::{Arc, Mutex},
+    thread::{self, JoinHandle},
 };
 
 /// Parallel CLI runner, similar to GNU parallel
@@ -10,7 +14,7 @@ struct Args {
     #[arg(long = "workers", default_value_t = 4)]
     workers: usize,
     /// Command template, use {} for input and {out} for optional safe filename
-    command: String,
+    template: String,
 }
 
 /// Sanitize a string to make a safe filename
@@ -21,69 +25,89 @@ fn sanitize_filename(input: &str) -> String {
         .collect()
 }
 
-fn main() {
-    let args = Args::parse();
-
+//  function to read all inputs
+fn read_inputs() -> Arc<Mutex<VecDeque<String>>> {
     let queue = Arc::new(Mutex::new(VecDeque::new()));
 
     let stdin = io::stdin();
 
     for paths in stdin.lock().lines() {
         match paths {
-            Ok(l) => queue.lock().unwrap().push_back(l),
-            Err(e) => eprintln!("Failed to read line:{}", e),
+            Ok(l) => {
+                queue.lock().unwrap().push_back(l);
+            }
+            Err(e) => {
+                eprintln!("Error while reading the from buffer: {}", e)
+            }
         }
     }
+    queue
+}
 
+fn worker_function(queue: Arc<Mutex<VecDeque<String>>>, template: String) {
+    loop {
+        let task = queue.lock().unwrap().pop_front();
+
+        let path = match task {
+            Some(l) => l,
+            None => break,
+        };
+
+        let output_filename = sanitize_filename(&path);
+        let cmd_final = template
+            .replace("{}", &path)
+            .replace("{out}", &output_filename);
+        let cmd_final_parts: Vec<&str> = cmd_final.split_whitespace().collect();
+        let program_command = cmd_final_parts[0];
+
+        // using "&" and borrowing as the compiler doesn't the size here
+        let program_arguments = &cmd_final_parts[1..];
+
+        let execution = process::Command::new(program_command)
+            .args(program_arguments)
+            .status();
+
+        match execution {
+            Ok(s) => {
+                eprintln!("[{}] exit: {:?}", path, s.code())
+            }
+            Err(e) => {
+                eprintln!("[{}] failed: {}", path, e)
+            }
+        }
+    }
+}
+
+fn spawn_workers(
+    count: usize,
+    queue: Arc<Mutex<VecDeque<String>>>,
+    template: String,
+    worker_function:fn(Arc<Mutex<VecDeque<String>>>, String)
+) -> Vec<JoinHandle<()>> {
     let mut handles = Vec::new();
-
-    println!("j value is {}" , args.workers);
-
-    for _ in 0..args.workers {
+    for _ in 0..count {
         let queue = Arc::clone(&queue);
-        let command_template = args.command.clone();
-
-        let handle = thread::spawn(move || {
-            loop {
-                let task = {
-                    let mut q = queue.lock().unwrap();
-                    q.pop_front()
-                };
-
-                let path = match task {
-                    Some(l) => l,
-                    None => break,
-                };
-
-                let output_filename = sanitize_filename(&path);
-
-                // building the command to execute
-                let cmd_final = command_template
-                    .replace("{}", &path)
-                    .replace("{out}", &output_filename);
-
-
-                let cmd_final_parts: Vec<&str> = cmd_final.split_whitespace().collect();
-
-                let program_cmd = cmd_final_parts[0];
-
-                //borrowing as we wouldn't know the size at compile time
-                let program_arguments  = &cmd_final_parts[1..];
-                
-                
-                match process::Command::new(program_cmd).args(program_arguments).status() {
-                Ok(s) => eprintln!("[{}] exit: {:?}", path, s.code()),
-                Err(err) => eprintln!("[{}] failed: {}", path, err),
-            }
-
-            }
-        });
-
+        let template_clone = template.clone();
+        let handle = thread::spawn(move || worker_function(queue, template_clone));
         handles.push(handle);
     }
+
+    handles
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let queue = read_inputs();
+
+    let handles = spawn_workers(args.workers, queue,args.template , worker_function);
+
+    println!("j = {}" , args.workers);
 
     // Wait for all threads to finish
     for handle in handles {
         let _ = handle.join();
     }
+
+    
 }
